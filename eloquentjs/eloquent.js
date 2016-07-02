f479bdf38cd44234ca1ffa56784c043f5f86352b
spiraldancing <spiraldancing@ymail.com>
2016-07-02 16:15:22 -0500

    Add Chapter 02 Exercise 02

--- eloquentjs/eloquent.js
+++ eloquentjs/eloquent.js
@@ -9,3 +9,19 @@ function chapter02Exercise01()
     console.log(nextLine + "\n");
   }
 }
+
+function chapter03Exercise02()
+{
+	// Your code here.
+	for (var i = 1; i <= 100; i++)
+	{
+	  if ((i % 3 == 0) && (i % 5 != 0))
+	    console.log("Fizz");
+	  else if ((i % 3 != 0) && (i % 5 == 0))
+	    console.log("Buzz");
+	  else if ((i % 3 == 0) && (i % 5 == 0))
+	    console.log("FizzBuzz");
+	  else
+	    console.log(i);
+	}
+}
